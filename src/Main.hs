59cae62566656757aa1bf5fdb71217dba5db4db4
Zaki Manian <zaki@reametrix.com>
2013-11-22 14:37:28 -0800

    Haskell single purpose server to target as webhook for updating a git repo

--- src/Main.hs
+++ src/Main.hs
@@ -5,20 +5,30 @@ import           Control.Applicative
 import           Snap.Core
 import           Snap.Util.FileServe
 import           Snap.Http.Server
+import           System.Posix.Process
+import           System.Posix.Signals
+import           System.Posix.Types
+import           System.Process
+import           System.IO
+import           Control.Applicative
+import           Control.Monad.Trans
+import           Control.Monad
+import qualified Data.ByteString.Char8 as C
 
 main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
 site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
+    ifTop (runGitPull)
 
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+
+runGitPull :: Snap()
+runGitPull = do
+    (inn, out, err, idd) <- liftIO $ runInteractiveCommand $ "git pull"
+    err_str <- liftIO $ hGetContents err
+    out_str <- liftIO $ hGetContents out
+    writeBS $ C.pack $ err_str ++ out_str
+
+
+
